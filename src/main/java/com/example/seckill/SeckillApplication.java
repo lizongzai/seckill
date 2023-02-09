4aa7039357b745ec4a5d80a45704a21ebf7cef82
root <lizongzai@gmail.com>
2023-02-09 10:49:57 +0800

    v1 项目框架构建、测试连接

--- src/main/java/com/example/seckill/SeckillApplication.java
+++ src/main/java/com/example/seckill/SeckillApplication.java
@@ -1,13 +1,15 @@
 package com.example.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.seckill.pojo")
 public class SeckillApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeckillApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SeckillApplication.class, args);
+  }
 
 }
--- src/main/java/com/example/seckill/controller/DemoController.java
+++ src/main/java/com/example/seckill/controller/DemoController.java
@@ -1,2 +1,29 @@
-package com.example.seckill.controller;public class DemoController {
+package com.example.seckill.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试
+ *
+ * @author lizongzai
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+  /**
+   * 功能描述: 测试页面跳转
+   *
+   * @param model
+   * @return
+   */
+  @RequestMapping("/hello")
+  public String hello(Model model) {
+    model.addAttribute("name", "xxxx");
+    return "hello";
+  }
+
 }
